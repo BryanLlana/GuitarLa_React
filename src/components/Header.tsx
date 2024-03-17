af3156c541a37c57b5a131fdabff0ab99914f2e8
Bryan Joseph <bryanllana14@gmail.com>
2024-03-17 13:34:27 -0500

    Header and ShoppingCart added

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import ShoppingCart from "./ShoppingCart"
+
+const Header = () => {
+  return (
+    <header className="py-5 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="index.html">
+              <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+            </a>
+          </div>
+          <ShoppingCart />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
