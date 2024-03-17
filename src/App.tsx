40de887afed432c680d8a67b913d8df9aaa653eb
Bryan Joseph <bryanllana14@gmail.com>
2024-03-17 14:29:50 -0500

    Delete state guitars because it not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react"
 import Guitars from "./components/Guitars"
 import Header from "./components/Header"
-import { Guitar } from "./data/interfaces/Guitar"
-import { guitars as data } from './data/guitars';
+import { guitars } from './data/guitars';
 
 const App = () => {
-  const [guitars, setGuitars] = useState<Guitar[]>(data)
 
   return (
     <>
